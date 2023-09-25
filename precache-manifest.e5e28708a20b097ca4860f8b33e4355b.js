5e9ad28edf709e01320cb97fd52781e3b6cb7697
ins25k <37101427+aimhigh53@users.noreply.github.com>
2023-09-25 20:28:28 +0900

    Updates

---
+++ precache-manifest.e5e28708a20b097ca4860f8b33e4355b.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f0215c84c71d1ee70c66f24ead32a97",
+    "url": "https://aimhigh53.github.io/gc/index.html"
+  },
+  {
+    "revision": "cea9461ecf4f4ac44451",
+    "url": "https://aimhigh53.github.io/gc/static/css/2.d5f4ed81.chunk.css"
+  },
+  {
+    "revision": "7bddd1fcfcf2909af8c6",
+    "url": "https://aimhigh53.github.io/gc/static/css/main.841a248d.chunk.css"
+  },
+  {
+    "revision": "cea9461ecf4f4ac44451",
+    "url": "https://aimhigh53.github.io/gc/static/js/2.bafb7be6.chunk.js"
+  },
+  {
+    "revision": "001c6d42e28d0992d8b53b17579fce1b",
+    "url": "https://aimhigh53.github.io/gc/static/js/2.bafb7be6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bddd1fcfcf2909af8c6",
+    "url": "https://aimhigh53.github.io/gc/static/js/main.ddf56913.chunk.js"
+  },
+  {
+    "revision": "4bbc0e8e04315385b759",
+    "url": "https://aimhigh53.github.io/gc/static/js/runtime-main.8727f598.js"
+  },
+  {
+    "revision": "afb909194fe1038fd974c8c910bb4bc4",
+    "url": "https://aimhigh53.github.io/gc/static/media/1chung.afb90919.png"
+  },
+  {
+    "revision": "86647c2f2fea6c932000e8e07f8a8a7d",
+    "url": "https://aimhigh53.github.io/gc/static/media/2chung.86647c2f.png"
+  },
+  {
+    "revision": "bf3f83c3d409c58a82aad12a22eae7ba",
+    "url": "https://aimhigh53.github.io/gc/static/media/gideon.bf3f83c3.png"
+  },
+  {
+    "revision": "2fdc90c29422ae18ad859081b907eb73",
+    "url": "https://aimhigh53.github.io/gc/static/media/hispeople.2fdc90c2.png"
+  },
+  {
+    "revision": "76c4992eee21ab3776d5be0ee21e4d6b",
+    "url": "https://aimhigh53.github.io/gc/static/media/main_1.76c4992e.png"
+  },
+  {
+    "revision": "af0da466789892a90afabf318656162d",
+    "url": "https://aimhigh53.github.io/gc/static/media/main_2.af0da466.png"
+  },
+  {
+    "revision": "73d45a5bcf12c5b255c8ad4cd1007c61",
+    "url": "https://aimhigh53.github.io/gc/static/media/media1.73d45a5b.png"
+  },
+  {
+    "revision": "614aaed8c10a2d466cdfd8287ab3ed13",
+    "url": "https://aimhigh53.github.io/gc/static/media/media2.614aaed8.png"
+  },
+  {
+    "revision": "3062f7829a585a21b08fd9c7aa0b0972",
+    "url": "https://aimhigh53.github.io/gc/static/media/media3.3062f782.png"
+  },
+  {
+    "revision": "ec83e9917c7837d7285521feb648d515",
+    "url": "https://aimhigh53.github.io/gc/static/media/media_platform_team.ec83e991.png"
+  },
+  {
+    "revision": "28b46b6ab726b84bd0b61338a88ddb84",
+    "url": "https://aimhigh53.github.io/gc/static/media/media_team.28b46b6a.png"
+  },
+  {
+    "revision": "189ff5b616a751d07e3bf62286ff2701",
+    "url": "https://aimhigh53.github.io/gc/static/media/new_family_team.189ff5b6.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "https://aimhigh53.github.io/gc/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "https://aimhigh53.github.io/gc/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "https://aimhigh53.github.io/gc/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "https://aimhigh53.github.io/gc/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "a8c0a78d7f1d24f66587ded499ef15ca",
+    "url": "https://aimhigh53.github.io/gc/static/media/social_team.a8c0a78d.png"
+  },
+  {
+    "revision": "5de9ec3cb727e53db07d9a78ffac295c",
+    "url": "https://aimhigh53.github.io/gc/static/media/welcome.5de9ec3c.png"
+  }
+]);
